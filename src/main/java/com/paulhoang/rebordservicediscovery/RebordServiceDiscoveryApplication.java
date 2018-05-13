0d90f52f6d811018c11cf59e9919c429b5979c49
General-Meow <spl.ivalis@gmail.com>
2018-05-13 23:03:32 +0100

    initial commit with plain start project

---
+++ src/main/java/com/paulhoang/rebordservicediscovery/RebordServiceDiscoveryApplication.java
@@ -0,0 +1,12 @@
+package com.paulhoang.rebordservicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebordServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebordServiceDiscoveryApplication.class, args);
+	}
+}
